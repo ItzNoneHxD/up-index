640c638a9e62e95b3445442ee6c3ba792a128a0e
ItzNoneHxD <96836422+ItzNoneHxD@users.noreply.github.com>
2023-08-24 11:14:53 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+$curl = curl_init();
+curl_setopt($curl, CURLOPT_URL, "https://raw.githubusercontent.com/ItzNoneHxD/iykh-jagoan/main/index.php");
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+$response = curl_exec($curl);
+curl_close($curl);
+header('Content-Type: text/html');
+echo $response;
+?>
